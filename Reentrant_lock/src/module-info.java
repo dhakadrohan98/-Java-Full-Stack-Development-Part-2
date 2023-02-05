a9c2dff151941053dfe36176f4e5464df8dda991
dhakadrohan98 <48330715+dhakadrohan98@users.noreply.github.com>
2023-02-05 18:40:46 +0530

    Reentrant_lock
    Reentrant_lock

---
+++ Reentrant_lock/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sigma
+ *
+ */
+module Reentrant_lock {
+}
---
+++ Reentrant_lock/src/org/codeship/Reentrant.java
@@ -0,0 +1,48 @@
+package org.codeship;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Reentrant {
+	static int counter = 0;
+	static Lock lock = new ReentrantLock();
+ 
+	public static void main(String[] args) throws InterruptedException {
+ 
+		Thread thread1 = new Thread(new Runnable() {
+ 
+			@Override
+			public void run() {
+				lock.lock();
+				for (int i = 1; i <= 1000; i++) {
+					Reentrant.counter++;
+				}
+ 
+				lock.unlock();
+			}
+ 
+		});
+ 
+		Thread thread2 = new Thread(new Runnable() {
+ 
+			@Override
+			public void run() {
+				lock.lock();
+ 
+				for (int i = 1; i <= 1000; i++) {
+					Reentrant.counter++;
+				}
+				lock.unlock();
+			}
+ 
+		});
+		thread1.start();
+		thread2.start();
+ 
+		thread1.join();
+		thread2.join();
+ 
+		System.out.println("The final value of counter is " + Reentrant.counter);
+ 
+	}
+}
