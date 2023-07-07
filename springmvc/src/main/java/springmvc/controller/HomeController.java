7f64079ed5ecf26fd0a700902571376bd31b0e0d
dhakadrohan98 <48330715+dhakadrohan98@users.noreply.github.com>
2023-07-08 00:56:20 +0530

    spirngmvc 1st commit
    spirngmvc 1st commit

---
+++ springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,21 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	//returing name of view
+	@RequestMapping("/home")
+	public String home() {
+		System.out.println("this is home url");
+		return "index";
+	}
+	
+	@RequestMapping("/about")
+	public String about() {
+		System.out.println("this is about controller");
+		return "about";
+	}
+}
