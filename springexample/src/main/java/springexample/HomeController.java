d225bfaf29677ec5ccf094b3a09ba2a48c4fde14
dhakadrohan98 <48330715+dhakadrohan98@users.noreply.github.com>
2023-08-15 01:37:01 +0530

    Interceptor concept is implemented
    Interceptor concept is implemented

---
+++ springexample/src/main/java/springexample/HomeController.java
@@ -0,0 +1,24 @@
+package springexample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String home() {
+		System.out.println("Landing to home page!!!");
+		return "home";
+	}
+	
+	//handling for request
+	@RequestMapping("/welcome")
+	public String welcome(@RequestParam("user") String name,Model m) {
+		System.out.println("User Name: "+name);
+		m.addAttribute("name", name);
+		return "welcome";
+	}
+}
